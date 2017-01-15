af757b4e85bd0b797e983fc830c6a7d9b7a29c45
FengLu <fenglu@google.com>
2017-01-15 23:36:31 +0000

    Revert "Revise gcp_setup script so that resource is created if non-exist."
    This reverts commit 7e1dddc5cea425e0badab94126fd97101f3ee0ed.

--- gcp_setup.sh
+++ gcp_setup.sh
@@ -2,38 +2,28 @@
 # 
 # Prepare GCP project environment by creating the required bucket and dataset.
 
-#############################################################################
-# Run the first command, return if succeed. Otherwise run the second command.
+#######################################################
+# Run command and check return status, exit if fails.
 # Arguments:
-#   $1: first command
-#   $2: second command
+#   $1: command to be run
 # Returns:
 #   None
-#############################################################################
-check_and_run() {
-  echo "Executing: "$1
-  unset IFS
+#######################################################
+exit_if_failed_on_command() {
+  echo "executing "$1
   $1
-  if [[ $? -ne 0 ]]; then
-    echo "Executing: "$2
-    $2
-    if [[ $? -ne 0 ]]; then
-      echo "Fail to set up the GCP environment."
-      exit 1
-    fi
+  if [[ $? -eq 0 ]]; then
+    echo "successful"
+  else
+    exit 1
   fi
-  echo "Success."
 }
 
 echo "Preparing GCP environment..."
-commands_to_run=(
-  "gsutil ls gs://airflow-gcp-smoke, gsutil mb gs://airflow-gcp-smoke"
-  "bq ls airflow, bq mk airflow"
-  "bq ls airflow_temp, bq mk airflow_temp" )
+commands_to_run=( "gsutil mb gs://airflow-gcp-smoke" "bq mk airflow"
+  "bq mk airflow_temp" )
 for command in "${commands_to_run[@]}"
 do
-  IFS=","
-  set $command
-  check_and_run $1 $2
+  exit_if_failed_on_command "$command"
 done
 echo "GCP environment is ready." 
